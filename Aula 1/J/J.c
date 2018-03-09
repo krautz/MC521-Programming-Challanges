f81b6ae4dd8bc6d26cba2ee334c07dae309a48e9
Caio Krauthamer <caio_k@hotmail.com>
2018-03-08 21:46:21 -0300

    adcionado relatorio e problema J

--- Aula 1/J/J.c
+++ Aula 1/J/J.c
@@ -1,32 +1,142 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Funcoes do MergeSort obtidas em https://www.geeksforgeeks.org/merge-sort/
+void merge(long int arr[], int l, int m, int r) {
+
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+    
+        if (L[i] <= R[j]) {
+        
+            arr[k] = L[i];
+            i++;
+        } else {
+        
+            arr[k] = R[j];
+            j++;
+        }
+        
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(long int arr[], int l, int r) {
+
+    if (l < r) {
+    
+        int m = l+(r-l)/2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+//Funcao binarysearch obtidas em https://www.geeksforgeeks.org/binary-search/
+
+long int binarySearch(long int arr[], int l, int r, long int x) {
+
+   if (r >= l) {
+        int mid = l + (r - l)/2;
+        
+        if (arr[mid] == x)  
+            return mid;
+ 
+        if (arr[mid] > x) 
+            return binarySearch(arr, l, mid-1, x);
+ 
+        return binarySearch(arr, mid+1, r, x);
+   }
+ 
+   return -1;
+}
+
+long int count_repeticoes (long int arr[], int ini, int fim, long int val, long int pos) {
+	
+	int i = pos - 1;
+	long int count = 1;
+	
+	while (arr[pos] == arr[i]) {
+		i--;
+		count++;
+	}
+	i = pos + 1;
+	while (arr[pos] == arr[i]) {
+		i++;
+		count++;
+	}
+	
+	return count;
+}
+
 int main () {
-    long int **matriz, count = 0;
-    int numero_linhas, i, j, k, m;
+    long int **matriz, *AeB, *CeD, count = 0, resp;
+    int numero_linhas, i, j, k;
     
     scanf("%d", &numero_linhas);
     
     matriz = malloc(numero_linhas * sizeof(long int *));
     for (i = 0; i < numero_linhas; i++)
         matriz[i] = malloc(4 * sizeof(long int));
+        
+    AeB = malloc (numero_linhas * numero_linhas * sizeof (long int));
+    CeD = malloc (numero_linhas * numero_linhas * sizeof (long int));
     
     for (i = 0; i < numero_linhas; i++) {
         scanf("%ld %ld %ld %ld", &matriz[i][0], &matriz[i][1], &matriz[i][2], &matriz[i][3]);
     }
     
-    for (i = 0; i < numero_linhas; i++) {
+    for (i = 0, k = 0; i < numero_linhas; i++) {
         for (j = 0; j < numero_linhas; j++) {
-            for (k = 0; k < numero_linhas; k++) {
-                for (m = 0; m < numero_linhas; m++) {
-                    if (matriz[i][0] + matriz[j][1] + matriz[k][2] + matriz[m][3] == 0) {
-                            count++;
-                    }
-                }
-            }
+        	AeB[k] = matriz[i][0] + matriz[j][1];
+        	k++;
         }
     }
     
+    for (i = 0, k = 0; i < numero_linhas; i++) {
+        for (j = 0; j < numero_linhas; j++) {
+        	CeD[k] = matriz[i][2] + matriz[j][3];
+        	k++;
+        }
+    }
+    
+    mergeSort (CeD, 0, k-1);
+    
+    for (i = 0; i < k; i++) {
+    	resp = binarySearch (CeD, 0, k-1, (-AeB[i]));
+    	
+    	if (resp != -1) {
+    		j = count_repeticoes (CeD, 0, k-1, (-AeB[i]), resp);
+    		count += j;
+    	}
+    }
+        	 
     printf("%ld\n", count);
     
     return 0;
---
+++ Aula 1/K/K.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Ponto {
+    double x;
+    double y;
+} ponto;
+
+typedef struct Saida {
+    int esq;
+    int dir;
+    int sup;
+    int inf;
+} saida;
+
+int main () {
+    int numero_pontos, i;
+    ponto *poligono, p;
+    saida resultado;
+
+    scanf("%d", &numero_pontos);
+
+    while (numero_pontos != 0) {
+
+        resultado.esq = 0;
+        resultado.dir = 0;
+        resultado.sup = 0;
+        resultado.inf = 0;
+
+        poligono = malloc (numero_pontos * sizeof (ponto));
+
+        for (i = 0; i < numero_pontos; i++) {
+            scanf("%lf %lf", &poligono[i].x, &poligono[i].y);
+        }
+        
+        scanf("%lf %lf", &p.x, &p.y);
+
+        for (i = 0; i < numero_pontos - 1; i++) {
+            if (poligono[i].x == poligono[i + 1].x) {
+                if ((poligono[i].y < p.y && poligono[i + 1].y > p.y) || (poligono[i].y > p.y && poligono[i + 1].y < p.y)) {
+                    if (poligono[i].x > p.x) {
+                        resultado.dir = 1;
+                    } else {
+                        resultado.esq = 1;
+                    }
+                }
+            } else if (poligono[i].y == poligono[i + 1].y) {
+                if ((poligono[i].x < p.x && poligono[i + 1].x > p.x) || (poligono[i].x > p.x && poligono[i + 1].x < p.x)) {
+                    if (poligono[i].y > p.y) {
+                        resultado.sup = 1;
+                    } else {
+                        resultado.inf = 1;
+                    }
+                }
+            }
+        }
+        
+        if (poligono[0].x == poligono[numero_pontos - 1].x) {
+            if ((poligono[0].y < p.y && poligono[numero_pontos - 1].y > p.y) || (poligono[0].y > p.y && poligono[numero_pontos - 1].y < p.y)) {
+                if (poligono[0].x > p.x) {
+                    resultado.dir = 1;
+                } else {
+                    resultado.esq = 1;
+                }
+            }
+        } else if (poligono[0].y == poligono[numero_pontos - 1].y) {
+            if ((poligono[0].x < p.x && poligono[numero_pontos - 1].x > p.x) || (poligono[0].x > p.x && poligono[numero_pontos - 1].x < p.x)) {
+                if (poligono[0].y > p.y) {
+                    resultado.sup = 1;
+                } else {
+                    resultado.inf = 1;
+                }
+            }
+        }
+
+        if (resultado.esq && resultado.dir && resultado.sup && resultado.inf) {
+            printf("T\n");
+        } else {
+            printf("F\n");
+        }
+        
+        free (poligono);
+        scanf("%d", &numero_pontos);
+    }
+    
+    return 0;
+}
