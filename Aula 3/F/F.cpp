5c8273b8cfe18a366fc8832b3489eceb91183c51
Caio Krauthamer <caio_k@hotmail.com>
2018-03-22 01:26:55 -0300

    adcionando o problema F corrigido

--- Aula 3/F/F.cpp
+++ Aula 3/F/F.cpp
@@ -3,13 +3,66 @@
 #include <algorithm>
 #include <cmath>
 #include <stdlib.h>
+#include <string>
 
 using namespace std;
 
-int main () {
-	int num_casos, p, q, caso = 1, first, flag, num, j, prox;
+long long int acha_menor (int p, int q) {
+	long long int i, pot = pow (2, q), conversao, j;
 	
-	long long int i, pot, maior, primeiro, copia;
+	string numero (p, '1');
+	numero[p-1] = '2';
+	
+	for (i = 0; i < pow (2, p-1); i++) {
+		conversao = stoll (numero);
+		if (conversao % pot == 0) {
+			return conversao;
+		}
+		
+		j = p - 2;
+		while (j >= 0) {
+			if (numero[j] == '1') {
+				numero[j] = '2';
+				j = -1;
+			} else {
+				numero[j] = '1';
+				j--;
+			}
+		}
+	}
+	return -1;
+}
+
+long long int acha_maior (int p, int q) {
+	long long int i, pot = pow (2, q), conversao, j;
+	
+	string numero (p, '2');
+	
+	for (i = 0; i < pow (2, p-1); i++) {
+		conversao = stoll (numero);
+		if (conversao % pot == 0) {
+			return conversao;
+		}
+		
+		j = p - 2;
+		while (j >= 0) {
+			if (numero[j] == '2') {
+				numero[j] = '1';
+				j = -1;
+			} else {
+				numero[j] = '2';
+				j--;
+			}
+		}
+	}
+	return -1;
+}
+				
+
+int main () {
+	int num_casos, p, q, caso = 1;
+	
+	long long int first, last;
 	
 	cin >> num_casos;
 	
@@ -21,58 +74,20 @@ int main () {
 		cout << "Case " << caso << ": ";
 		caso++;
 		
+		first = acha_menor (p, q);
 		
-		pot = pow (2, q);
-		vector<int> bin (p, 0);
-		bin[0] = 1;
-		for (first = 1, i = 0; i < pow(2, p-1); i++) {
-			
-			num = 0;
-			for (j = 0; j < bin.size(); j++) {
-				if (bin[j] == 0) {
-					num += 1*pow(10,j);
-				} else if (bin[j] == 1) {
-					num += 2*pow(10,j);
-				}
-			}
-
-			j = 0;
-			flag = 1;
-			while (flag != 0) {
-				if (j == bin.size() - 1) {
-					prox = 0;
-				} else {
-					prox = j + 1;
-				}
-				if (bin[prox] == 1) {
-					bin[prox] = 0;
-					j++;
-				} else {
-					bin[prox] = 1;
-					flag--;
-				}
-			}
-	
-			if (num % pot == 0) {
-				if (first == 1) {
-					cout << num;
-					first = 0;
-					primeiro = num;
-				} 
-				maior = num;
-			}
-		}
-		
-		if (first == 1) {
+		if (first == -1) {
 			cout << "impossible\n";
-		} else if (maior != primeiro) {
-			cout << " " << maior << "\n";
-		} else  if (maior == primeiro) {
-			cout << "\n";	
+		} else {
+			last = acha_maior (p, q);
+			if (last == first) {
+				cout << first << "\n";
+			} else {
+				cout << first << " " << last << "\n";
+			}
 		}
 		num_casos--;
 	}
 	
 	return 0;
 }
-		
